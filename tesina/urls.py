c2eaeab9b020e419d7a2c5d7e6f5ab8e994176a5
jorgeluis11 <jorgeluis@ubuntu.(none)>
2013-05-03 20:13:58 -0400

    heroku final deploy

--- tesina/urls.py
+++ tesina/urls.py
@@ -62,3 +62,7 @@ urlpatterns += patterns('',
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
     'document_root': settings.MEDIA_ROOT}))
 
+urlpatterns += patterns('',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+     {'document_root': settings.STATICFILES_DIRS[0], 'show_indexes': True}),
+)
